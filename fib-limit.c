0239c1c6c73aa5fd56e27fe33699b2f33b842099
RHHEN122 <rokyhenderson222@gmail.com>
2025-05-01 17:21:35 +1200

    Added C-LIMIT, Changed C-OP

--- fib-limit.c
+++ fib-limit.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int a = 0, b = 1;
+int a = 0, b = 1, i = 0;
+int limit = 2;
 
 int main() {
     while (1) {
         a += b;
         printf(a, "\n");
+        i += 1;
         b += a;
         printf(b, "\n");
+        i += 1;
+        if (i == limit) {
+            break;
+        }
     }
 }
